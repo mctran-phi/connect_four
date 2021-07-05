77d584542941be395b6defb9e32d26d22b7f6814
Minh Tran <mctran98@yahoo.com>
2021-07-05 15:48:32 -0700

    Implementd checkRows and checkColumns and displayed winner condition

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,39 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import Table from './Table.jsx';
 import $ from 'jquery';
-import { reset, dropPiece } from '../../utils/functions.js';
+import { reset, dropPiece, checkRows, checkColumns, checkDiagonals } from '../../utils/functions.js';
 
 export default function App() {
   const [player, setPlayer] = useState(1);
+  const [rounds, setRounds] = useState(1);
+  const [winner, setWinner] = useState(false);
 
   useEffect(() => {
     $('.cell').click(e => {
-      if (!$(e.target).hasClass('yellow') && !$(e.target).hasClass('red')) {
-        if (player === 1) {
-          // $(e.target).addClass('yellow');
-          dropPiece(e.target, 'yellow');
-          setPlayer(2);
-        } else {
-          // $(e.target).addClass('red');
-          dropPiece(e.target, 'red');
-          setPlayer(1);
+      if (!winner) {
+        if (!$(e.target).hasClass('yellow') && !$(e.target).hasClass('red')) {
+          if (player === 1) {
+            dropPiece(e.target, 'yellow', '1');
+            if (checkRows(e.target, '1') || checkColumns(e.target, '1') || checkDiagonals(e.target, '1')) {
+              setWinner(true);
+            } else {
+              setPlayer(2);
+            }
+          } else {
+            dropPiece(e.target, 'red', '2');
+            if (checkRows(e.target, '2') || checkColumns(e.target, '2') || checkDiagonals(e.target, '2')) {
+              setWinner(true);
+            } else {
+              setPlayer(1);
+            }
+          }
         }
-        console.log(e.target);
       }
     });
 
     return () => $('.cell').off('click');
 
-  }, [player]);
+  }, [player, rounds]);
 
   function handleReset() {
     setPlayer(1);
+    setWinner(false);
+    setRounds(prev => prev);
     reset();
   };
 
   return (
     <div className='container'>
       <h2>Connect Four</h2>
+      {winner && <div className='winner'>Player {player} Won!</div>}
       <Table player={player}/>
       <div className='footer'>
         {player === 1 && <div>Turn: <span className='player1'>Player 1</span></div>}
--- utils/functions.js
+++ utils/functions.js
@@ -1,21 +1,61 @@
 const $ = require('jquery');
 
 function reset() {
-  $('.cell').removeClass('yellow red');
+  $('.cell').removeClass('yellow red').val('0');
 };
 
-function dropPiece(element, color) {
+function dropPiece(element, color, value) {
   var col = element.classList[2];
 
   for (var row = 5; row >= 0; row--) {
-    if (!($(loc).hasClass('red') ^ $(loc).hasClass('yellow'))) {
-      $(loc).addClass(color);
+    var current = $(`.row${row}.${col}`);
+    if (!($(current).hasClass('red') ^ $(current).hasClass('yellow'))) {
+      $(current).addClass(color).val(value);
       break;
     }
   }
 };
 
+function checkRows(element, value) {
+  var row = element.classList[1];
+  var count = 0;
+
+  for (var col = 0; col < 7; col++) {
+    if ($(`.${row}.col${col}`).val() === value) {
+      count++;
+      console.log(element, count);
+    } else {
+      count = 0;
+    }
+    if (count === 4) break;
+  }
+  // return false;
+};
+
+function checkColumns(element, value) {
+  var col = element.classList[2];
+  var count = 0;
+
+  for (var row = 0; row < 6; row++) {
+    if ($(`.row${row}.${col}`).val() === value) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count === 4) return true;
+  }
+  return false;
+};
+
+function checkDiagonals(element) {
+  var row = element.classList[1];
+  var col = element.classList[2];
+};
+
 module.exports = {
   reset: reset,
-  dropPiece: dropPiece
+  dropPiece: dropPiece,
+  checkRows: checkRows,
+  checkColumns: checkColumns,
+  checkDiagonals, checkDiagonals
 };
